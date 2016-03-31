515359af96c135ceae6620ffa9134d9c1c9e594a
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2016-03-31 02:09:00 +0200

    style: simple arrow fn

--- euler-16.js
+++ euler-16.js
@@ -1,10 +1,8 @@
-const powerDigitSum = n => {
-  return Math.pow(2, n)
+const powerDigitSum = n => Math.pow(2, n)
   .toString()
   .split('')
   .map(c => parseInt(c))
   .reduce((r, n) => r + n)
-}
 
 // works for 15, but returns NaN for 1000
 console.log(powerDigitSum(15))
