1777732f9631b25064553ff94bd472d2052bd1e3
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2016-04-01 11:00:46 +0200

    feat: switch to Fedor Indutny's BN
    BigNumber.js has some minor inconsistencies:
    
    - POW_PRECISION needs to be set to 0 to enable correct powers
    - toString() switches to e representation for larger numbers

--- euler-16.js
+++ euler-16.js
@@ -1,28 +1,13 @@
 
 'use strict'
 
-const assert = require('assert')
-// Not to be confused with 'bignumber'
-const BigNumber = require('bignumber.js')
-
-// For whatever reason, power operations need to be configured
-BigNumber.config({ POW_PRECISION: 0 })
+const BN = require('bn.js')
 
 // :: Number -> Number
-// works for n=15, but returns NaN for n=1000
-const powerDigitSumBroke = n => Math.pow(2, n)
+const euler16 = n => new BN(2).pow(new BN(n))
   .toString()
-  .split('')
-  .map(c => parseInt(c))
-  .reduce((r, n) => r + n)
-
-// console.log(powerDigitSum(15))
-
-const powerDigitSum = n => new BigNumber(2).pow(n)
-  // toString() might return exp represenation .toString()
-  .toFixed()
   .split('')
   .map(c => parseInt(c))
   .reduce((r, n) => r + n)  
 
-console.log(powerDigitSum(1000))
+console.log(euler16(1000))
