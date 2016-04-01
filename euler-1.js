e8856c04b104f22ae76d3cb03242a1a67911c578
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2016-04-01 11:04:10 +0200

    feat: switch to testable function

--- euler-1.js
+++ euler-1.js
@@ -1,6 +1,6 @@
 const I = require('immutable')
 
-console.log(
-  I.Range(1, 1000)
+// :: -> Number
+const euler1 = _ => I.Range(1, 1000)
   .filter(n => n % 3 === 0 || n % 5 === 0)
-  .reduce((r, n) => r + n))
+  .reduce((r, n) => r + n)
