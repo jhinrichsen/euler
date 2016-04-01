b63591bb3ef87b2ef2a1e8230df67e72f8b4c4ee
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2016-04-01 16:23:25 +0200

    refactor(euler19): replace generator function with immutable's 'Range' permutations

--- euler.js
+++ euler.js
@@ -2,35 +2,33 @@
 
 const BN = require('bn.js')
 const I = require('immutable')
+const R = require('ramda')
+
 
 // :: -> Number
-const euler1 = _ => I.Range(1, 1000)
-  .filter(n => n % 3 === 0 || n % 5 === 0)
-  .reduce((r, n) => r + n)
+const euler1 = _ =>
+  I.Range(1, 1000)
+    .filter(n => n % 3 === 0 || n % 5 === 0)
+    .reduce((r, n) => r + n)
 
 // :: Number -> Number
-const euler16 = n => new BN(2).pow(new BN(n))
-  .toString()
-  .split('')
-  .map(c => parseInt(c))
-  .reduce((r, n) => r + n)
-
-// :: -> [Date]
-function * dates () {
-  for (let year = 1901; year <= 2000; year++) {
-    // Months are 0 based as in Java
-    for (let month = 0; month < 12; month++) {
-      // Days are 1 based
-      yield new Date(year, month, 1)
-    }
-  }
-}
+const euler16 = n =>
+  new BN(2).pow(new BN(n))
+    .toString()
+    .split('')
+    .map(c => parseInt(c))
+    .reduce((r, n) => r + n)
 
 // :: -> Number
-// Can't find a way to construct Immutable.Seq(generator function)
-const euler19 = _ => [...dates()]
-  .filter(date => date.getDay() === 0)
-  .reduce((r, n) => r + 1, 0)
+const euler19 = _ =>
+  // permutations are called xprod in Ramda.js 
+  R.xprod(I.Range(1901, 2001).toArray(),
+          // Month is 0 based (as in Java)
+          I.Range(0, 12).toArray())
+    // Days are 1 based (again, as in Java)
+    .map(ym => new Date(...ym, 1))
+    .filter(date => date.getDay() === 0)
+    .reduce(r => r + 1, 0)
 
 exports.euler1 = euler1
 exports.euler16 = euler16
