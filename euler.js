41b0047a4ed7053c8ae0474239a277fc99d0afb2
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2016-04-03 00:11:12 +0200

    feat: Add Euler #92

--- euler.js
+++ euler.js
@@ -46,8 +46,52 @@ const euler48 = () => I.Range(1, 1001)
   .toString(10)
   .slice(-10)
 
+// :: Number -> Number
+const squareDigit = (n) => n
+  .toString()
+  .split('')
+  .map((n) => n * n)
+  .reduce((r, n) => r + n)
+
+// :: Number -> [Number]
+function * squareDigitChain (n) {
+  do {
+    yield n
+    n = squareDigit(n)
+  } while (n !== 1 && n !== 89)
+  yield n
+}
+
+// :: Number -> Number
+// Slow: takes about 90 seconds on a 2013 MacBook Pro
+const euler92_ = (n) => I.Range(1, n)
+  .map((n) => I.Seq(squareDigitChain(n)).last())
+  // 10 % faster, but mutation going on:
+  // .map((n) => [...squareDigitChain(n)].pop())
+  .filter((n) => n === 89)
+  .count()
+
+// :: Number -> boolean
+// Highly specialized, but still takes 60s
+const squareDigitsChainEndsIn89 = (n) => {
+  for (;;) {
+    if (n === 89) return true
+    if (n === 1) return false
+    n = squareDigit(n)
+  }
+}
+
+// :: Number -> Number
+const euler92 = (n) => I.Range(1, n)
+  .map((n) => squareDigitsChainEndsIn89(n))
+  .filter((b) => b)
+  .count()
+
 exports.euler1 = euler1
 exports.euler16 = euler16
 exports.euler19 = euler19
 exports.euler20 = euler20
 exports.euler48 = euler48
+exports.squareDigit = squareDigit
+exports.squareDigitChain = squareDigitChain
+exports.euler92 = euler92
--- test/euler-test.js
+++ test/euler-test.js
@@ -5,6 +5,9 @@ const assert = require('assert')
 const Euler = require('../')
 
 describe('Euler', function () {
+  // Euler92 takes minutes
+  this.timeout(1000000)
+  
   describe('#euler1()', function () {
     it('solves Project Euler #1', function () {
       assert.equal(233168, Euler.euler1())
@@ -30,4 +33,31 @@ describe('Euler', function () {
       assert.equal('9110846700', Euler.euler48())
     })
   })
+  describe('#squareDigit(44)', function () {
+    it('returns 32 for the sum of square digits of 44', function () {
+      assert.equal(32, Euler.squareDigit(44))
+    })
+  })
+  describe('#squareDigit(85)', function () {
+    it('returns 89 for the sum of square digits of 85', function () {
+      assert.equal(89, Euler.squareDigit(85))
+    })
+  })
+  describe('#squareDigitChain(44)', function () {
+    it('returns square digit chain value 44', function () {
+      assert.deepEqual([ 44, 32, 13, 10, 1 ], [...Euler.squareDigitChain(44)])
+    })
+  })
+  describe('#squareDigitChain(85)', function () {
+    it('returns square digit chain value 85', function () {
+      assert.deepEqual([ 89, 145, 42, 20, 4, 16, 37, 58, 89 ],
+        [...Euler.squareDigitChain(89)])
+    })
+  })
+  describe('#euler92()', function () {
+    it('solves Project Euler #92', function () {
+      assert.equal(8581146, Euler.euler92(1e7))
+    })
+  })
 })
+
